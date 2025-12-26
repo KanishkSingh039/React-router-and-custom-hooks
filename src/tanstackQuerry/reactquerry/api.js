ae6f5bb10c5105bd365c06b17d5e93aef8c8a68a
Kanishk Singh <kanishks039@gmail.com>
2025-12-27 01:25:54 +0530

    learn about the react

--- src/tanstackQuerry/reactquerry/api.js
+++ src/tanstackQuerry/reactquerry/api.js
@@ -23,3 +23,46 @@ const productlist = [{
         id: 6
     },
 ]
+
+export const Fetchlistofproduct = async() => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return productlist;
+};
+// A Promise is an object that represents:
+// “Something that will finish later”
+// It has 3 states:
+// Pending – still working
+// Fulfilled – success
+// Rejected – error
+
+// Meaning of resolve():
+//  “My work is DONE successfully”
+// When you call resolve()
+// Promise moves from pending → fulfilled
+// await stops waiting
+// Code continues
+
+//Step-by-step:
+// Promise is created → pending
+// setTimeout starts a 1-second timer
+// After 1 second → resolve() is called
+// Promise becomes fulfilled
+// await continues execution
+
+
+export const addnewproduct = async(ProductName) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000)); // purpose of this line :To pause the function for 1 second in an async/await–friendly way.  
+    //Function waits 1 second
+    // Promise stays pending
+    // UI can show loading
+    // Behaves like a real network request
+    //and then later is will do thge further task like here it is creating the newproduct in product list and above returning or acting like fatching the productlist
+    const newlycreatedproduct = {
+        id: productlist.length + 1,
+        title: ProductName
+    };
+    productlist.push(newlycreatedproduct);
+    return newlycreatedproduct;
+}
+
+//now what will happen we have to rapp the app comonent in querry client provider compoenent just like we have created context provider , now further we have to edit the main file
--- src/tanstackQuerry/reactquerry/index.jsx
+++ src/tanstackQuerry/reactquerry/index.jsx
@@ -1,8 +1,33 @@
+import { useQuery } from "@tanstack/react-query";
+import { Fetchlistofproduct } from "./api";
+
 function ReactQuerry()
 {
+    // const query=useQuery({
+    //     queryKey:['productlist'],//this is provide name or to identify what are we doing
+    //     queryFn: ()=>Fetchlistofproduct(),  //this is to tell what dunction we need to call
+    // })
+    // console.log(query);
+    // console.log(query.data);
+    const {data,isLoading}=useQuery(
+        {
+            queryKey:['productlist'],
+            queryFn:()=>Fetchlistofproduct()
+        }
+    )
+    if(isLoading)return <h1>Loading!!!</h1>
     return(
         <div>
             <h1>Tanstack Querry</h1>
+            <ul>
+                
+                {
+                    data?.length?data.map(product=>{
+                        console.log(product);
+                        return(<li key={product.id}>{product.title}</li>)
+                    }):<h1>Sorry!!!! not able to fetch data</h1>
+                }
+            </ul>
         </div>
     )
 }
